619b1444df68b2c3f5798647fad7a11ab9bbc767
Nick <nickolay.stoyanov.95@gmail.com>
2018-08-08 06:47:15 +0300

    Created login screen. Minor fixes.

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var loginRouter = require('./routes/login');
-var usersRouter = require('./routes/users');
-
 var app = express();
+
+var loginRouter = require('./routes/login');
+var startRouter = require('./routes/start');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', loginRouter);
-app.use('/users', usersRouter);
+app.use('/', startRouter);
+app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('login', { title: 'Express' });
+router.post('/', function(req, res, next) {
+  
+  var uname = req.body["username"];
+  var upass = req.body["password"];
+
+  console.log(uname, upass)
+
+  // check if both have values
+  if (uname != "" && upass != "") {
+
+    // check if there is a match in the database
+    MongoClient.connect('mongodb://localhost:27017/', {useNewUrlParser: true}, function (err, client) {
+      
+      users = client.db("kingdomino").collection("users")
+      
+      users.find({"username":uname, "password":upass}).count(function (err, result) {
+          if (err) throw err     
+
+          // there is a match, log in the person
+          if (result == 1) res.render("success");
+          
+          // there is no match, try again
+          else res.render("fail");
+
+      })
+    })
+  }
+
+  // either password or username is invalid, require another attemp to login
+  else {res.render('fail');}
+
 });
 
 module.exports = router;
